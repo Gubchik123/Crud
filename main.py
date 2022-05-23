9d47c1d75d6e61981f0c988a5cb7266bd8d96c2d
Nikita <nikita.gubarev31@gmail.com>
2022-05-23 18:51:32 +0300

    I fixed some lines which connected with getters and setters of my class. And added some comments for readable

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from class_Person import Person
-from time import sleep
-from os import system as sys
+from class_Person import Person  # Importing class from my module
+from time import sleep           # Importing special function for waiting
+from os import system as sys     # Importing special function for clearing console
 
 
+# Function for asking user about his\her wanting to continue
 def start():
     print("--- Crud-приложение ---".center(40))
     answer = input("Войти в систему? (Да или нет)\nВаш выбор: ")
-    sys("cls")
+    sys("cls")  # Clear console
     if answer.lower() == "да":
         menu()
     elif answer.lower() == "нет":
@@ -16,6 +17,7 @@ def start():
         print_bye()
 
 
+# Function for choosing future actions
 def menu():
     while True:
         print("--- Меню ---".center(40))
@@ -24,7 +26,7 @@ def menu():
         print("3 - Удалить студента(ов) из группы")
         print("0 - Выйти")
         answer = int(input("Ваш выбор: "))
-        sys("cls")
+        sys("cls")  # Clear console
         if answer == 1:
             show_list_of_students()
         elif answer == 2:
@@ -38,6 +40,7 @@ def menu():
             there_is_no_such_type_of_answer()
 
 
+# Function for showing students list
 def show_list_of_students():
     while True:
         if len(students) == 0:
@@ -49,7 +52,7 @@ def show_list_of_students():
         print("2 - Изменить детальную информацию")
         print("0 - Вернуться назад")
         answer = int(input("Ваш выбор: "))
-        sys("cls")
+        sys("cls")  # Clear console
 
         if answer == 1:
             info_about_student()
@@ -61,31 +64,33 @@ def show_list_of_students():
             there_is_no_such_type_of_answer()
 
 
+# Function for showing information about some student
 def info_about_student():
     students_list()
     answer = int(input("\nВведите номер студента про которого хотите увидеть информацию: "))
-    sys("cls")
+    sys("cls")  # Clear console
     if answer < 1 or answer > len(students):
         there_is_no_such_student_num()
     else:
         while True:
             for stud in students:
-                if stud.get_number() == answer:
+                if stud.number == answer:
                     stud.print_info()
 
             print("\n0 - Вернуться назад")
             answer = int(input("Ваш выбор: "))
-            sys("cls")
+            sys("cls")  # Clear console
             if answer == 0:
                 break
             else:
                 there_is_no_such_type_of_answer()
 
 
+# Function for changing some information about student
 def change_info_about_student():
     students_list()
     answer = int(input("\nВведите номер студента информацию которого хотите поменять: "))
-    sys("cls")
+    sys("cls")  # Clear console
     if answer < 1 or answer > len(students):
         there_is_no_such_student_num()
     else:
@@ -99,14 +104,14 @@ def change_info_about_student():
             print("5 - Поменять номер компьютера")
             print("0 - Вернуться назад")
             choise = int(input("Ваш выбор: "))
-            sys("cls")
+            sys("cls")  # Clear console
             if choise == 0:
                 break
             elif choise < 0 or choise > 5:
                 there_is_no_such_type_of_answer()
             else:
                 for stud in students:
-                    if stud.get_number() == answer:
+                    if stud.number == answer:
                         if choise == 1:
                             stud.enter_name()
                         elif choise == 2:
@@ -120,6 +125,7 @@ def change_info_about_student():
                 wait_and_clear()
 
 
+# Function for adding student(s)
 def add_students():
     print("--- Добавление студента(ов) ---".center(40))
     count = int(input("Введите сколько студентов хотите добавить: "))
@@ -131,7 +137,7 @@ def add_students():
             print(f"--- Студент №{i + 1} ---".center(40))
             global person
             person = Person()
-            person.set_number(i + 1)
+            person.number = i + 1
             person.enter_name()
             person.enter_surname()
             person.enter_age()
@@ -144,6 +150,7 @@ def add_students():
         wait_and_clear()
 
 
+# Function for deleting student(s)
 def del_students():
     while True:
         if len(students) == 0:
@@ -153,12 +160,12 @@ def del_students():
         print("\n1 - Удалить одного студента"
               "\n2 - Удалить всех студентов")
         answer = int(input("Ваш выбор: "))
-        sys("cls")
+        sys("cls")  # Clear console
 
         if answer == 1:
             students_list()
             answer = int(input("\nВведите номер студента которого хотите удалить: "))
-            sys("cls")
+            sys("cls")  # Clear console
             if answer < 1 or answer > len(students):
                 there_is_no_such_student_num()
             else:
@@ -166,7 +173,7 @@ def del_students():
                 sys("cls")
                 if choice.lower() == "да":
                     for stud in students:
-                        if stud.get_number() == answer:
+                        if stud.number == answer:
                             students.remove(stud)
                             print("Студент был успешно удалён!".center(40))
                             wait_and_clear()
@@ -180,7 +187,7 @@ def del_students():
 
         elif answer == 2:
             choice = input("Вы действительно хотите удалить всех студентов? (Да или нет)\nВаш выбор: ")
-            sys("cls")
+            sys("cls")  # Clear console
             if choice.lower() == "да":
                 students.clear()
                 print("\n")
@@ -199,21 +206,24 @@ def del_students():
         break
 
 
+# Function for printing students list
 def students_list():
     print("--- Список студентов ---".center(40))
     i = 1
     for stud in students:
-        stud.set_number(i)
+        stud.number = i
         print(stud)
         i += 1
 
 
+# Function for showing message that action canceled
 def operation_canceled():
     print("\n")
     print("Операция отменена!".center(40))
     wait_and_clear()
 
 
+# Function for showing message that user entered invalid answer and action canceled
 def there_is_no_such_type_of_answer_and_operation_canceled():
     print("\n")
     print("Такого варианта ответа нет".center(40))
@@ -221,11 +231,13 @@ def there_is_no_such_type_of_answer_and_operation_canceled():
     wait_and_clear()
 
 
+# Function for waiting and clearing
 def wait_and_clear():
-    sleep(3)   # Задержка 3 секунды
-    sys("cls") # Очистка терминала
+    sleep(3)    # Wait 3 seconds
+    sys("cls")  # Clear console
 
 
+# Function for showing message that user entered invalid answer
 def there_is_no_such_type_of_answer():
     print("\n")
     print("Такого варианта ответа нет!".center(40))
@@ -233,12 +245,14 @@ def there_is_no_such_type_of_answer():
     wait_and_clear()
 
 
+# Function for showing message that the list of student is empty
 def list_is_empty():
     print("\n")
     print("Список студентов пуст!".center(40))
     wait_and_clear()
 
 
+# Function for showing message that user entered invalid student number
 def there_is_no_such_student_num():
     print("\n")
     print("Студента с таким номером нет!".center(40))
@@ -246,11 +260,13 @@ def there_is_no_such_student_num():
     wait_and_clear()
 
 
+# Function for showing message "Good bye!"
 def print_bye():
     print("\nДо свидания!")
 
 
+# Start of our program
 if __name__ == "__main__":
-    person = Person()
-    students = list()
-    start()
+    person = Person()   # Object of my class for future actions with it
+    students = list()   # List for saving all students
+    start()  # Start
